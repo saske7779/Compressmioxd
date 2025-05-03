129c21e74ced5d81a50173f882495c811a21968e
saske7779 <hubcuentagit@gmail.com>
2025-05-03 16:58:55 -0400

    Actualizar main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ import re
 # Configuración del bot
 API_ID = '21282861'
 API_HASH = '5570ce56a170e27183b728b887f88aa0'
-BOT_TOKEN = '8040530669:AAH2-RroQAT0RjDOVjE6dnTybFri61S5cb4'
+BOT_TOKEN = '7895848873:AAFVpb-dkOi6BuniX_w7xWYLD3kvqT3qIXw'
 
 # Lista de administradores supremos (IDs de usuario)
 SUPER_ADMINS = [5702506445]
